5c86ef0adef9509efd53671d9f4f8e4109a41f46
Glitch (earthy-brazil) <none>
2018-08-19 17:05:13 +0000

    🌽🏰 Checkpoint ./server.js:1588544/190 ./package.json:1588544/4393

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+var bodyParser=require('body-parser');
 
 var cors = require('cors');
 
@@ -12,12 +13,13 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGO_URI);
 
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -35,3 +37,7 @@ app.get("/api/hello", function (req, res) {
 app.listen(port, function () {
   console.log('Node.js listening ...');
 });
+
+
+app.route('/name')
+  .post((req,res)=> res.json({name:req.body.first + ' '+req.body.last}))
