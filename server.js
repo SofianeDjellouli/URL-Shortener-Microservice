6cf3f0ea2156a091151b66e53d9bc09da57d209a
Glitch (url-shortener-microservice-) <none>
2018-09-06 19:11:08 +0000

    comments added

--- server.js
+++ server.js
@@ -9,10 +9,8 @@ var cors = require('cors');
 
 var app = express();
 
-// Basic Configuration 
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
 mongoose.connect(process.env.MONGO_URI);
 var Schema=mongoose.Schema;
 var urlSchema = new Schema({
@@ -22,62 +20,55 @@ var urlSchema = new Schema({
 var Url = mongoose.model('Url', urlSchema);
 
 app.use(cors());
-
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
 app.use(bodyParser.urlencoded({extended: false}));
-
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-
 app.listen(port, function () {
   console.log('Node.js listening ...');
 });
 
-
 app.route('/api/shorturl/new')
   .post((req,res)=> {  
+//   we change the requested url so it can be parsed
     if (req.body.url.slice(0,7)==='http://'){
       req.body.url=req.body.url.slice(7);
     } else if (req.body.url.slice(0,8)==='https://'){
       req.body.url=req.body.url.slice(8);
     }
+//   we check if the url is valid
     dns.lookup(req.body.url,(err,add)=>{
       if (err) {
         res.json({"error":"invalid URL"})
       } else {
+//         if the shortened url is already cready, we return it
         Url.findOne({original_url: req.body.url},(er,data)=>{
-        if (er) console.log(er);
-        else if (data) {
-          console.log(data);
-          res.json({"original_url": req.body.url,
-                    "short_url":data.short_url});
-        } else {
-          console.log(data);
-          Url.create({original_url: req.body.url,
-                      short_url:Math.floor(Math.random() * 1000) + 1}
-                      ,(error,dat)=>{
-                        if(error) console.log(error);
-                        else res.json({"original_url": req.body.url,
-                                      "short_url":dat.short_url});
-                        });
-          }  
+          if (er) res.json(er);
+          else if (data) {
+            res.json({"original_url": req.body.url,
+                      "short_url":data.short_url});
+          } 
+//           otherwise, we create one another and return it
+//           we set the max number of urls created to 1000
+          else {
+            Url.create({original_url: req.body.url,
+                        short_url:Math.floor(Math.random() * 1000) + 1}
+                        ,(error,dat)=>{
+                          if(error) res.json(error);
+                          else res.json({"original_url": req.body.url,
+                                        "short_url":dat.short_url});
+                          });
+            }  
         });
       }
     });
   });
 
+// the redirect view
 app.get('/api/shorturl/:url',(req,res)=> {
   Url.findOne({short_url:req.params.url},(err,data)=>
-              res.redirect('https://'+data.original_url))
+              res.redirect('http://'+data.original_url))
 });
